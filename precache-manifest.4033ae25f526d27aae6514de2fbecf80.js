de1ca38e3fcb7dbfb46ae58d7c5c85507f83d193
Ahmad Iqbal Hafizi <ahmad.hafizi@tuhh.de>
2018-11-11 12:00:01 +0100

    Updates

---
+++ precache-manifest.4033ae25f526d27aae6514de2fbecf80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "be8e2d18ba8157b96df4",
+    "url": "/todo/static/js/main.be8e2d18.chunk.js"
+  },
+  {
+    "revision": "b36c5939986e1f29f72d",
+    "url": "/todo/static/js/1.b36c5939.chunk.js"
+  },
+  {
+    "revision": "be8e2d18ba8157b96df4",
+    "url": "/todo/static/css/main.ec015de8.chunk.css"
+  },
+  {
+    "revision": "d6cdb934903c81937fdedccf0734905f",
+    "url": "/todo/index.html"
+  }
+];
